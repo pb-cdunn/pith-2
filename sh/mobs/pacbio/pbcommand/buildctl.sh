3cfb90599868fdaa20f06478fdf9ff5ab78ca772
Christopher Dunn <cdunn@pacificbiosciences.com>
2015-08-18 21:57:32 -0700

    direct copies from mobs

---
+++ sh/mobs/pacbio/pbcommand/buildctl.sh
@@ -0,0 +1,309 @@
+#!/bin/bash
+
+# ---- required subroutines
+set_globals() {
+    # required globals:
+    g_name="pbcommand"
+
+    # FIXME: unused for now...
+    # # other globals:
+    # g_outdir_name="_output";
+    # g_outdir_abs="$(readlink -m "$g_progdir/../$g_outdir_name")"
+    # g_outdir=$g_outdir_abs;
+    # g_installbuild_dir_abs="${g_outdir}/install-build"
+    # g_installbuild_dir="${g_installbuild_dir_abs}"
+
+    g_rootdir_reltop="bioinformatics/lib/python/pbcommand";
+
+    g_topdir="$g_progdir/../../../../.."
+    g_topdir_abs=$(readlink -f "$g_topdir");
+
+    g_outdirtop_abs="$g_topdir_abs/_output/modulebuilds"
+	
+    g_srcdir_abs="$g_topdir_abs/$g_rootdir_reltop"
+    g_outdir_abs="$g_outdirtop_abs/$g_rootdir_reltop/_output"
+
+    g_git_rootdir="$g_outdirtop_abs/prebuilt.tmpout/git/git_2.4.5/libc-2.5/gcc-4.8.4/_output/install"
+    g_git_exe="${g_git_rootdir}/binwrap-build/git"
+
+    g_git_remote_url=$(bash -c '. "'"$g_srcdir_abs/${g_name}"'.github.ish"; echo "$URL"')
+    g_git_ref=$(bash -c '. "'"$g_srcdir_abs/${g_name}"'.github.ish"; echo "$REF"')
+
+    local git_dirname=$(basename "$g_git_remote_url" .git)
+    g_git_fetch_dir="$g_outdir_abs/build"
+    g_git_build_topdir="$g_git_fetch_dir/$git_dirname"
+    g_git_build_srcdir="$g_git_build_topdir"
+}
+
+# ---- build targets
+
+fetch_gitsrc() {
+    local git_home=$g_git_fetch_dir;
+    local git_destdir="$g_git_build_topdir"
+
+    local remoteinfo;
+    local mustclone=false;
+    local stat;
+    stat=0;
+    if [[ ! -e "$git_destdir/.git" ]] ; then
+	mustclone=true;
+    else
+	remoteinfo=$(HOME="$git_home" "$g_git_exe" -C "$git_destdir" remote show origin) || stat=$?
+	if [[ $stat -eq 0 ]] ; then
+	    local fetchurl;
+	    fetchurl=$(echo "$remoteinfo" | sed -ne 's/^[[:space:]]*Fetch URL:[[:space:]]*//p')
+	    if [[ x"$fetchurl" != x"$g_git_remote_url" ]] ; then
+		mustclone=true;
+	    fi
+	elif [[ $stat -eq 1 ]] ; then
+	    mustclone=true;
+	else
+	    merror "Unexpected error (1) when running git command (exitstat: $stat)"
+	fi
+    fi
+
+    if $mustclone; then
+	echo "Cloning $g_name sources from git..."
+	rm -rf "$git_home"
+	mkdir -p "$git_home"
+	HOME="$git_home" "$g_git_exe" clone "$g_git_remote_url" "$git_destdir"
+    fi
+
+    local mustfetch=false;
+    # This will check to see if the reference is actually a valid git commint
+    # sha hash in the local repository.  If so, we don't need to go out and 
+    # fetch the latest from the remote repository.  For any other ref type
+    # (e.g. HEAD, tag, branch,...) we cannot be guaranteed that it didnt't 
+    # change on the remote side, so we need to fetch anyway.  If it not a 
+    # reference listed by 'show-ref', but it is a git commit sha hash listed 
+    # with parse-rev, but it not in the local repository, we also need
+    # to fetch the latest.  This rev-parse command should handle all the cases
+    # (no need to fetch if exits with zero status, fetch otherwise).
+    # See this link for more info on testing refs:
+    #    http://stackoverflow.com/questions/18222634/given-a-git-refname-can-i-detect-whether-its-a-hash-tag-or-branch
+    stat=0
+    HOME="$git_home" "$g_git_exe" -C "$git_destdir" show-ref "${g_git_ref}" > /dev/null 2>&1 || stat=$?
+    if [[ $stat -eq 0 ]] ; then    
+	# This is a HEAD, tag, branch, remote reference.  We will need to 
+	# fetch (since it is a pointer that may have been updated in the
+	# remote repository.
+	mustfetch=true;
+    elif [[ $stat -eq 1 ]] ; then
+	stat=0
+	HOME="$git_home" "$g_git_exe" -C "$git_destdir" rev-parse "${g_git_ref}^{commit}" > /dev/null 2>&1 || stat=$?
+	if [[ $stat -ne 0 ]] ; then
+	    # This is either not a valid sha hash, or it is one that our 
+	    # local repository does not know about yet.  In either case
+	    # fetch it from the remote repository.
+	    mustfetch=true;
+	fi
+    else
+	merror "Unexpected error (2) when running git command (exitstat: $stat)"
+    fi
+
+    if $mustfetch; then
+	echo "Fetching (updating) $g_name sources from git..."
+	HOME="$git_home" "$g_git_exe" -C "$git_destdir" fetch
+    else
+	echo "Git sources for $g_name already up to date..."
+    fi
+
+    # Now do a sanity check to make sure we actually have the ref in question
+    # in our local repository
+    stat=0
+    HOME="$git_home" "$g_git_exe" -C "$git_destdir" rev-parse "${g_git_ref}^{commit}" > /dev/null 2>&1 || stat=$?
+    if [[ $stat -ne 0 ]] ; then
+	merror "Could not find the git reference '$g_git_ref' after updating the local repository"
+    fi
+
+    # Now update to the specified reference or sha hash
+    # FIXME: temporary hack to avoid reset if this file exists.  Not a good
+    #        long term solution (since developers could lose edits and 
+    #        cannot easily test equivalent of nightly build).  See bug 26971.
+    if [[ ! -e "$g_srcdir_abs/no-git-reset.txt" ]] ; then
+	HOME="$git_home" "$g_git_exe" -C "$git_destdir" reset --hard "$g_git_ref"
+    fi
+
+    # At this point we should have the expected code in $git_destdir
+}
+
+clean() {
+    echo "Running $g_name 'clean' target..."
+    bash ${g_progdir}/pkgbuild.sh --clean
+}
+cleanall() {
+    echo "Running $g_name 'cleanall' target..."
+    clean;
+}
+build() {
+    fetch_gitsrc;
+
+    echo "Running $g_name 'build' target..."
+    bash ${g_progdir}/pkgbuild.sh --build
+}
+install_build() {
+    if ! $opt_no_sub_targets; then
+	build;
+    fi
+
+    echo "Running $g_name 'install-build' target..."
+    bash ${g_progdir}/pkgbuild.sh --install
+}
+install_prod() {
+    echo "Running $g_name 'install-prod' target..."
+}
+publish_build() {
+    if ! $opt_no_sub_targets; then
+	install_build;
+    fi
+
+    echo "Running $g_name 'publish-build' target..."
+
+}
+publish_prod() {
+    if ! $opt_no_sub_targets; then
+	install_prod;
+    fi
+    echo "Running $g_name 'cleanall' target..."
+
+}
+
+
+# ---- End of Module-specific code
+# Common code from here on out, do not modify...
+
+# ---- error handling
+set -o errexit;
+set -o posix;
+set -o pipefail;
+set -o errtrace;
+unexpected_error() {
+    local errstat=$?
+    echo "${g_prog:-$(basename $0)}: Error! Encountered unexpected error at 'line $(caller)', bailing out..." 1>&2
+    exit $errstat;
+}
+trap unexpected_error ERR;
+
+
+g_prog=$(basename $0);
+g_progdir=$(dirname $0);
+
+# ---- usage
+
+usage() {
+  local exitstat=2;
+  if [[ ! -z "$2" ]] ; then
+      exitstat=$2;
+  fi
+
+  # Only redirect to stderr on non-zero exit status
+  if [[ $exitstat -ne 0 ]] ; then
+      exec 1>&2;
+  fi
+
+  if [[ ! -z "$1" ]] ; then
+      echo "$g_prog: Error! $1" 1>&2;
+  fi
+
+  echo "Usage: $g_prog [--help] \\"
+#  echo "              -t|--target buildtarget";
+#  echo "         -t|--target     -- chef target to build (e.g. 'cookbookname::build')";
+  echo "         --help          -- print this usage";
+  echo "";
+
+  # bash only:
+  if [[ $exitstat -ne 0 ]] ; then
+      echo "  at: $(caller)";
+  fi
+  exit $exitstat;
+}
+
+# ---- argument parsing
+
+# Save off the original args, use as "${g_origargs[@]}" (with double quotes)
+declare -a g_origargs;
+g_origargs=( ${1+"$@"} )
+
+opt_target_exist_check=false;
+opt_no_sub_targets=false;
+opt_process_all_deps=false;
+declare -a opt_additional_options;
+declare -a opt_targets;
+while [[ $# != 0 ]]; do
+    opt="$1"; shift;
+    case "$opt" in
+	# Flag with no argument example:
+	#   --flag|--fla|--fl|--f)
+	#     opt_flag=true;;
+	# Option with argument example:
+	#   --arg|--ar|--a)
+	#     [[ $# -eq 0 ]] && usage;
+	#     opt_somearg=$1; shift;;
+	-e|--exists|--exist-check|--target-exist-check) opt_target_exist_check=true;;
+	-s|--no-sub|--no-subs|--no-sub-targets|--single) opt_no_sub_targets=true;;
+	-d|--deps|--process-all-deps|--all-deps|-all) opt_process_all_deps=true;;
+	-o) 
+	    [[ $# -eq 0 ]] && usage;
+	    opt_additional_options=( "${opt_additional_options[@]}" "$1" );
+	    shift;;
+	-h|-help|--help|--hel|--he|--h) usage "" 0;;
+	--*) opt_targets=( "${opt_targets[@]}" "$opt" );;
+	-*) usage "Unrecognized option: $opt";;
+	*)  usage "Extra trailing arguments: $opt $@";;
+    esac
+done
+
+# ---- error functions
+merror() {
+    echo "$g_prog: Error! ""$@" 1>&2;
+    exit 1;
+}
+minterror() {
+    echo "$g_prog: Internal Error! ""$@" 1>&2;
+    exit 1;
+}
+mwarn() {
+    echo "$g_prog: Warning! ""$@" 1>&2;
+}
+
+# ---- globals
+
+# ---- subroutines
+
+munge_target() {
+    local target=$1; shift;
+    local mtarget=$target;
+    
+    mtarget=${mtarget#--}
+    mtarget=${mtarget//-/_}
+    echo "$mtarget"
+}
+
+# ---- main
+
+set_globals;
+
+warnings=false;
+for target in "${opt_targets[@]}"; do
+    mtarget=$(munge_target "$target");
+    if ! declare -f -F "$mtarget" > /dev/null; then
+	if $opt_strict; then
+	    mwarn "target '$target' does not exist"
+	    warnings=true;
+	else
+	    echo "$g_prog: target '$target' does not exist"
+	fi
+    fi
+done
+if $warnings; then
+    merror "Detected warnings, bailing out..."
+fi	
+
+if ! $opt_target_exist_check; then
+    for target in "${opt_targets[@]}"; do
+	mtarget=$(munge_target "$target");
+	eval "$mtarget" "${opt_additional_options[@]}"
+    done
+fi
+
+exit 0;
---
+++ sh/mobs/pacbio/pbcommand/pkgbuild.sh
@@ -0,0 +1,292 @@
+#!/bin/bash
+
+# ---- error handling
+set -o errexit;
+set -o posix;
+set -o pipefail;
+set -o errtrace;
+unexpected_error() {
+    local errstat=$?
+    echo "${g_prog:-$(basename $0)}: Error! Encountered unexpected error at 'line $(caller)', bailing out..." 1>&2
+    exit $errstat;
+}
+trap unexpected_error ERR;
+
+# Force the path to only what we need, saving off the original path
+PATH_ORIG=$PATH;
+PATH=/usr/bin:/bin
+
+g_prog=$(basename "$0");
+g_progdir=$(dirname "$0");
+g_progdir_abs=$(readlink -f "$g_progdir");
+
+# ---- error functions
+merror() {
+    echo "$g_prog: Error! ""$@" 1>&2;
+    exit 1;
+}
+minterror() {
+    echo "$g_prog: Internal Error! ""$@" 1>&2;
+    exit 1;
+}
+mwarn() {
+    echo "$g_prog: Warning! ""$@" 1>&2;
+}
+
+
+# ---- usage
+
+usage() {
+  local exitstat=2;
+  if [[ ! -z "$2" ]] ; then
+      exitstat=$2;
+  fi
+
+  # Only redirect to stderr on non-zero exit status
+  if [[ $exitstat -ne 0 ]] ; then
+      exec 1>&2;
+  fi
+
+  if [[ ! -z "$1" ]] ; then
+      echo "$g_prog: Error! $1" 1>&2;
+  fi
+
+  echo "Usage: $g_prog [--help] \\"
+  echo "              [--clean] [--build] [--install]";
+  echo "         --clean       -- clean the build (def: clean if no args specified)";
+  echo "         --build       -- build the build (def: build if no args specified)";
+  echo "         --install     -- install the build (def: install if no args specified)";
+  echo "         --help        -- print this usage";
+  echo "";
+
+  # bash only:
+  if [[ $exitstat -ne 0 ]] ; then
+      echo "  at: $(caller)";
+  fi
+  exit $exitstat;
+}
+
+# ---- argument parsing
+
+# Save off the original args, use as "${g_origargs[@]}" (with double quotes)
+declare -a g_origargs;
+g_origargs=( ${1+"$@"} )
+
+opt_clean=false;
+opt_build=false;
+opt_install=false;
+while [[ $# != 0 ]]; do
+    opt="$1"; shift;
+    case "$opt" in
+	# Flag with no argument example:
+	#   --flag|--fla|--fl|--f)
+	#     opt_flag=true;;
+	# Option with argument example:
+	#   --arg|--ar|--a)
+	#     [[ $# -eq 0 ]] && usage;
+	#     opt_somearg=$1; shift;;
+	--clean) opt_clean=true;;
+	--build) opt_build=true;;
+	--install) opt_install=true;;
+	-h|-help|--help|--hel|--he|--h) usage "" 0;;
+	-*) usage "Unrecognized option: $opt";;
+	*)  usage "Extra trailing arguments: $opt $@";;
+    esac
+done
+
+if ! $opt_clean && ! $opt_build && ! $opt_install; then
+    # No args specified, perform all the actions
+    opt_clean=true;
+    opt_build=true;
+    opt_install=true;
+fi
+
+# ---- global
+
+# --start module specific settings
+g_pkgname="pbcommand"
+g_module_dir_reltop="_output/modulebuilds/bioinformatics/lib/python/pbcommand/_output/build/pbcommand/"
+
+g_builddeps=""
+g_builddeps="$g_builddeps setuptools_14.3.1"
+g_builddeps="$g_builddeps pip_6.0.8"
+g_builddeps="$g_builddeps wheel_0.24.0"
+
+g_requires_gcc=false;
+g_requires_gxx=false;
+g_requires_ar=false;
+# --end module specific settings
+
+g_topdir="$g_progdir/../../../../.."
+g_topdir_abs=$(readlink -f "$g_topdir");
+g_module_dir="$g_topdir/$g_module_dir_reltop";
+g_module_dir_abs="$g_topdir_abs/$g_module_dir_reltop";
+
+g_pip_rootdir=""
+for builddep in $g_builddeps; do
+    if [[ $builddep =~ ^pip_ ]] ; then
+	pipver=${builddep##*_};
+	g_pip_rootdir="${g_topdir_abs}/prebuilt.tmpsrc/pythonpkgs/pip/pip_${pipver}/_output/install"
+    fi
+done
+if [[ -z "$g_pip_rootdir" ]]; then
+    merror "No pip build dependency specified."
+fi
+
+g_installdeps=""
+for builddep in $g_builddeps; do
+    if [[ $builddep =~ ^pip_ ]] ; then
+	g_installdeps="$g_installdeps $builddep";
+    elif [[ $builddep =~ ^wheel_ ]] ; then
+	g_installdeps="$g_installdeps $builddep";
+    fi
+done
+
+g_python_exe="${g_topdir_abs}/prebuilt.out/python/python-2.7.3/centos-5/bin/python"
+g_python_ldlibpath="${g_topdir_abs}/prebuilt.out/installerdeps/installerdeps-pacbio-0.1/centos-5/lib"
+
+g_srcroot_dir="$g_module_dir"
+g_srcroot_dirabs="$g_module_dir_abs"
+g_output_dir="$g_srcroot_dir/_output"
+g_output_dirabs="$g_srcroot_dirabs/_output"
+g_wheel_dir="$g_output_dir/wheelhouse"
+
+# ---- main
+
+if $opt_clean; then
+    # Clean the output dir
+    echo "Cleaning _output dir..."
+    rm -rf "$g_output_dir"
+fi
+
+
+if $opt_build; then
+    # Make a copy of the source tree and put it in _output/src.  We will 
+    # actually hardlink to the source using "rsync --link-dest".  We will
+    # invoke "pip wheel"  pointing to the linked source below.
+    # NOTE: this is needed because the "pip wheel" command below copies all 
+    #       the source into a tmp directory, and we are setting TMPDIR to 
+    #       _output/src in our build tree (to avoid pip polluting /tmp with
+    #       temporary dirs from aborted builds and such).  That combination
+    #       will end up in an endless recursion of copying the source tree 
+    #       (in the python shutil.copytree() function) because it copies the
+    #       _output directory to _output/tmp (and there is no way, via 
+    #       "pip wheel", to specify excluding particular directories)
+    echo "Linking source dir..."
+    mkdir -p "${g_output_dir}/src"
+    rsync -a --exclude "_output" --link-dest="${g_srcroot_dirabs}"  "${g_srcroot_dir}/" "${g_output_dir}/src"
+    
+    l_ldflags=""
+    l_ldshared=""
+    l_cc=""
+    l_cxx=""
+    l_ar=""
+    if $g_requires_gcc || $g_requires_gxx; then
+	g_gcc_rootdir="${g_topdir_abs}/prebuilt.out/3.x/gcc/gcc-4.8.4/libc-2.5"
+
+	if $g_requires_gcc; then
+            # Querying python for LDFLAGS
+            echo "Querying python for LDFLAGS..."
+            ldflags=$(LD_LIBRARY_PATH="$g_python_ldlibpath" \
+                     "$g_python_exe" \
+                     -c 'import sysconfig; print(sysconfig.get_config_var("LDSHARED"))')
+            # Remove first arg in ldflags, presumably 'gcc'
+            ldflags=${ldflags#* }
+
+	    l_ldflags=$ldflags
+	    l_cc="$g_gcc_rootdir/bin/gcc"
+	    l_ldshared="$l_cc"
+	fi
+	if $g_requires_gxx; then
+	    l_cxx="$g_gcc_rootdir/bin/g++"
+	fi
+	if $g_requires_ar; then
+	    l_ar="$g_gcc_rootdir/bin/ar"
+	fi
+    fi
+
+    g_builddeps_pythonpath=""
+    for builddep in $g_builddeps; do
+	pkg=${builddep%_*}
+	pkgver=${builddep##*_}
+	g_builddeps_pythonpath="${g_builddeps_pythonpath}:${g_topdir_abs}/prebuilt.tmpsrc/pythonpkgs/${pkg}/${pkg}_${pkgver}/_output/install/lib/python2.7/site-packages"
+    done
+    g_builddeps_pythonpath=${g_builddeps_pythonpath#:}
+
+    if [[ x$(type -t module_build_specialcase) == x"function" ]] ; then
+	module_build_specialcase;
+    fi
+
+    # Creating wheel
+    # NOTE: We are setting TMPDIR to a directory under _output to avoid 
+    #       littering /tmp with old directories from failed builds and such.
+    #       See note above in "Linking source dir..." for more details.
+    echo "Creating the ${g_pkgname} wheel from source..."
+    mkdir -p "${g_output_dir}/tmp"
+    TMPDIR="${g_output_dirabs}/tmp" \
+    PYTHONPATH="${g_builddeps_pythonpath}" \
+    LD_LIBRARY_PATH="$g_python_ldlibpath" \
+    LDFLAGS="$l_ldflags" \
+    LDSHARED="$l_ldshared" \
+    CC="$l_cc" \
+    CXX="$l_cxx" \
+    AR="$l_ar" \
+	"$g_python_exe" \
+	    "${g_pip_rootdir}/bin/pip" \
+                wheel --no-clean --no-index --no-deps  --build "$g_output_dir/build" --src "$g_output_dir/src" --wheel-dir "$g_wheel_dir" --verbose "${g_output_dir}/src"
+fi
+
+
+if $opt_install; then
+    g_installdeps_pythonpath=""
+    for installdep in $g_installdeps; do
+	pkg=${installdep%_*}
+	pkgver=${installdep##*_}
+	g_installdeps_pythonpath="${g_installdeps_pythonpath}:${g_topdir_abs}/prebuilt.tmpsrc/pythonpkgs/${pkg}/${pkg}_${pkgver}/_output/install/lib/python2.7/site-packages"
+    done
+    g_installdeps_pythonpath=${g_installdeps_pythonpath#:}
+
+    if [[ x$(type -t module_install_specialcase) == x"function" ]] ; then
+	module_install_specialcase;
+    fi
+
+    # Use the PYTHONUSERBASE/--user method for installing a python package into
+    # a different directory, as suggested here:
+    #
+    #   http://stackoverflow.com/questions/2915471/install-a-python-package-into-a-different-directory-using-pip/29103053#29103053
+    # 
+    # Note that other methods don't really give us what we want:
+    #   - the --root option prepends the absolute path to the python executable
+    #     after the specified rootdir (seems to to the right thing in 
+    #     installing the bin, lib, share dirs, but it is inconvenient have 
+    #     the full path to the python binary in output dir).
+    #   - the --target method only installs the lib directory at the target, 
+    #     but not the full bin, lib, share structure we want (the scripts in 
+    #     bin are missing).
+    #   - the --install-option="--prefix=$(pwd)/_output/install" dir option 
+    #     seems to be ignored.  It always seems to instal in the site-packages
+    #     dir of the python tree of the python executable.
+    # Note that even this method does not give us exactly what we want:
+    #   - the --user option will assign mode 700 to all the directories and 
+    #     files (except for the bin directory).  This will make it so that 
+    #     other users will not be able to run programs properly.  So we will 
+    #     use this method and force the proper ownership after the "pip 
+    #     install" command.
+
+    # Now use the version of pip (installed into _output/build above) to create
+    # a pip-only install dir (without setuptools).  Note, must specify the 
+     # site-packages dir of the install build output above  in the PYTHONPATH.
+    echo "Installing ${g_pkgname} (from wheel)..."
+    PYTHONUSERBASE="${g_output_dirabs}/install" \
+    PYTHONPATH="$g_installdeps_pythonpath" \
+    LD_LIBRARY_PATH="$g_python_ldlibpath" \
+	"$g_python_exe" \
+	    "${g_pip_rootdir}/bin/pip" \
+                install --no-index --find-links="$g_wheel_dir" --no-deps --user --ignore-installed "$g_pkgname"
+
+    # Add group and user permisssions, since the --user option to 'pip install'
+    # forces 700 permissions on the entire install tree (with the exception of
+    # 'install/bin').
+    echo "Adding group/other read permissions..."
+    chmod -R og=u-w "${g_output_dirabs}/install"
+fi
